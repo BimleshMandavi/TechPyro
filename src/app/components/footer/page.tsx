3fa98f5a894c9bf3569337d1c9224ef647657205
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-04-21 00:20:44 +0530

    first commit

---
+++ src/app/components/footer/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import React from 'react'
+
+const FooterPage = () => {
+  return (
+    <div className='bg-[#121212] w-[428px] h-[111px] top-[6842px] left-[1px] text-white flex justify-between'>
+      <div>
+       <div></div>
+        <div>About Us</div>
+      </div>
+      <div>Blogs</div>
+      <div>Home</div>
+      <div>Carrier</div>
+      <div>Contact</div>
+    </div>
+  )
+}
+
+export default FooterPage
