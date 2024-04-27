8362ef5943b3e397a9e6cf278c6495e6f38d04e5
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-04-28 04:38:56 +0530

    commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ const AboutPage = () => {
     <div className=' sm:h-full'>
       <div className="relative h-[640px] w-full bg-custom-bg bg-cover bg-center text-white">
       <div className="absolute inset-0 bg-black bg-opacity-85" />
-      <div className="relative flex items-center  h-full  ml-4 text-start">
+      <div className="relative flex items-center  h-full  ml-4 text-start sm:px-8">
         <div className="flex justify-between flex-col gap-1 mx-6">
         <h4 className="text-[14px] text-[#FF7F0E]">TECH PYRO</h4>
         <h2 className="text-4xl font-bold">About Us</h2>
@@ -21,19 +21,20 @@ const AboutPage = () => {
         </div>
       </div>
     </div>
-    <div>
+    <div className=''>
       <div className='h-[305px] bg-[#000000] text-white '>
-         <div className='flex flex-col gap-5 px-6'>
+         <div className='flex flex-col gap-5 px-6 sm:px-8 ml-4 '>
           <h1 className='font-bold text-[28px]'>The Tech Pyro Way</h1>
-          <p>The tech pyro Way  is a tech hiring platform that helps recruiters and engineering managers effortlessly hire the best developers thanks to a powerful suite of virtual</p>
-          <p >The tech pyro Way  is a tech hiring platform that helps recruiters and engineering managers effortlessly hire the best developers thanks to a powerful suite of virtual</p>
+          <div className='sm:flex'>
+          <p className='sm:w-[735px] sm:p-5'>Creating the tech behind building great tech teams has taught us that the key to real, lasting success is always the people. So we’ve built a culture of acceptance where introverts and extroverts, early birds and night owls, and left brainers as well as right brainers feel at home.</p>
+          <p className='sm:w-[674px] sm:p-5'>The tech pyro Way  is a tech hiring platform that helps recruiters and engineering managers effortlessly hire the best developers thanks to a powerful suite of virtual</p>
+          </div>
          </div>
-      
-      <div className='pt-8 pl-5'>
-        <Image src='/leaders.svg' alt='lead' height={132} width={496} />
+      <div className='pt-8 pl-5 sm:w-full'>
+        <Image className='sm:w-[1614px] sm:h-[538px] p-8' src='/leaders.svg' alt='lead' height={132} width={496} />
       </div>
       </div>
-      <div className='h-[1123px] bg-[#000000] text-white'>
+      <div className='h-[1123px] sm:h-[873px] bg-[#000000] text-white sm:pt-[273px] sm:px-7 sm:flex'>
         <div className='pt-[210px] flex flex-col gap-3 px-5'>
           <h4 className='text-[11px] text-[#FF7F0E]'>BACKGROUND</h4>
          <div className='text-[28px]'> <h1 className='font-bold '>The purpose</h1>
@@ -41,12 +42,12 @@ const AboutPage = () => {
           <p>Tech pyro is a tech hiring platform that helps recruiters and engineering managers effortlessly hire the best developers thanks to a powerful suite of virtual recruiting tools that help identify, assess, interview and engage developers.</p>
           <p>The current program will run through 2022, with participants divided into four cohorts.</p>
         </div>
-        <div className='pt-12 px-5 flex flex-col gap-4'>
-          <div className='text-[28px]'>
+        <div className='pt-12 px-5 flex flex-col gap-4 sm:pt-[243px]'>
+          <div className='text-[28px] sm:flex sm:gap-3'>
             <h1 className='font-bold'>Who</h1>
             <h1>are we looking For?</h1>
           </div>
-          <ul className="flex flex-col gap-4 list-none pl-5 relative">
+          <ul className="flex flex-col gap-4 list-none pl-5 relative sm:flex-row sm:p-7 ">
       <li className="relative before:content-['•'] before:absolute before:left-[-1.5rem] before:text-red-500 before:text-xl">
         <div>
           <p className="font-bold pb-3">Proactive leaders</p>
@@ -72,15 +73,15 @@ const AboutPage = () => {
         </div>
       </li>
     </ul>
-        </div>
-      </div>
-      <div className='bg-[#0C487B]  h-[609px] text-white py-5 px-5 flex justify-around flex-col'>
-        <div>
+ </div>
+ </div>
+      <div className='bg-[#0C487B]  h-[609px] sm:h-[438px] text-white py-5 px-5 flex justify-around flex-col sm:flex-row'>
+        <div className='sm:w-[707px]'>
           <h5 className='text-[12px] font-semibold'>OUR MISSION</h5>
           <h1 className='font-bold text-[28px]'>Creating Valuable Content For Creatives All  The World</h1>
           <p>ThinkHumble is a tech hiring platform that helps recruiters and engineering managers effortlessly hire the best developers thanks to a powerful suite of virtual recruiting tools that help identify, assess, interview and engage developers.</p>
         </div>
-       <div>
+       <div className='sm:w-[741px]'>
           <h5 className='text-[12px] font-semibold'>OUR VISION</h5>
           <h1 className='font-bold text-[28px]'>A Platform That Empowers Individuals To Improve</h1>
           <p>ThinkHumble is a tech hiring platform that helps recruiters and engineering managers effortlessly hire the best developers thanks to a powerful suite of virtual recruiting tools that help identify, assess, interview and engage developers.</p>
@@ -89,47 +90,49 @@ const AboutPage = () => {
     </div>
     <div>
       <div className='bg-[#050505]  h-[1011px] flex flex-col  items-center text-white gap-3'>
+        <div className='sm:flex sm:gap-5'>
         <div className='text-center pt-9'>
-          <Image src="/mission1.svg" alt='mission' width={366} height={245} />
+          <Image className='sm:w-[641px] sm:h-[518px]' src="/mission1.svg" alt='mission' width={366} height={245} />
         </div>
-        <div className='w-[358px]'>
-          <h1 className='text-[25px] font-bold'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+        <div className='w-[358px] sm:w-[784px] sm:pt-10 sm:pl-6'>
+          <h1 className='text-[25px] font-bold sm:text-[50px] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
           <p  className='text-[14px]'>ThinkHumble is a tech hiring platform that helps recruiters and engineering managers effortlessly hire the best developers thanks to </p>
+        <div>
+          <Image className='sm:w-[894px]' src="/mission2.svg" alt='mission' width={366} height={245} />
+        </div>
+        </div>
         </div>
         <div>
-          <Image src="/mission2.svg" alt='mission' width={366} height={245} />
-        </div>
-        <div>
-            <Image src="/mission3.svg" alt='mission' width={366} height={245} />
+            <Image className='sm:mr-[845px] sm:w-[641px] sm:h-[293px]' src="/mission3.svg" alt='mission' width={366} height={245} />
         </div>
       </div>
-      <div className='bg-[#050505]  h-[1211px] text-white flex flex-col items-center px-8'>
+      <div className='bg-[#050505]  h-[1211px] sm:h-[345px]  text-white flex flex-col  px-8 '>
         <div className='text-[28px]'>
           <h1 className='font-bold'>We give our people everything</h1> <span>that’s needed to succeed</span>
         </div>
-        <ul className="flex flex-col gap-4 list-none pl-5 relative">
-          <div className='pt-6'><Image src="std.svg" width={50} height={49.9} alt='grp'/></div>
+        <ul className="flex flex-col gap-4 list-none pl-5 relative sm:flex-row sm:pt-8">
+          <Image src="std.svg" width={50} height={49.9} alt='grp'/>
       <li className="relative before:content-['•'] before:absolute before:left-[-1.5rem] before:text-red-500 before:text-xl">
         <div>
           <h1 className="font-bold pb-3 text-[18px]">Learning</h1>
           <p className='text-[14px]'>Our goal as an organisation is to be open about where the boundary of our knowledge lies and to hire people smarter than ourselves to push this boundary outward. We’ve learned over the years that saying.</p>
         </div>
       </li>
-      <div className='pt-6'><Image src="grp-icon.svg" width={50} height={49.9} alt='grp'/></div>
+      <Image src="grp-icon.svg" width={50} height={49.9} alt='grp'/>
       <li className="relative before:content-['•'] before:absolute before:left-[-1.5rem] before:text-red-500 before:text-xl">
         <div> 
           <h1 className="font-bold pb-3 text-[18px]">Leadership</h1>
           <p className='text-[14px]'>Our goal as an organisation is to be open about where the boundary of our knowledge lies and to hire people smarter than ourselves to push this boundary outward. We’ve learned over the years that saying.</p>
         </div>
       </li>
-      <div className='pt-6'><Image src="trs.svg" width={50} height={49.9} alt='grp'/></div>
+     <Image src="trs.svg" width={50} height={49.9} alt='grp'/>
       <li className="relative before:content-['•'] before:absolute before:left-[-1.5rem] before:text-red-500 before:text-xl">
         <div>
           <h1 className="font-bold pb-3 text-[18px]">Transparency</h1>
           <p className='text-[14px]'>Our goal as an organisation is to be open about where the boundary of our knowledge lies and to hire people smarter than ourselves to push this boundary outward. We’ve learned over the years that saying.</p>
         </div>
       </li>
-      <div className='pt-6'><Image src="frd.svg" width={50} height={49.9} alt='grp'/></div>
+      <Image src="frd.svg" width={50} height={49.9} alt='grp'/>
       <li className="relative before:content-['•'] before:absolute before:left-[-1.5rem] before:text-red-500 before:text-xl">
         <div>
           <h1 className="font-bold pb-3 text-[18px]">Transparency</h1>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
 
 const ContactPage = () => {
   return (
     <div>
-      <h1>Contact page</h1>
+      <div className="relative h-full w-full bg-custom-bg bg-cover bg-center text-white">
+      <div className="absolute inset-0 bg-black bg-opacity-80" />
+     <div className='relative flex flex-col items-center '>
+       <div className=' bg-black w-[358px] h-[559px] m-14 text-white'>
+        <div className='text-center pt-5'>
+          <h1 className='text-[24px] font-black'>Get In Tuch</h1>
+           <Image className='pt-3' src="rtc1.svg" width={383} height={1} alt='map' />
+        </div>
+      </div>
+      <div>
+        <Image src="map.svg" width={368} height={211} alt='map' />
+      </div>
+     </div>
+    </div>
     </div>
   )
 }
