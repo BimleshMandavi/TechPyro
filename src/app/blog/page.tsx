5e97ce6281d6d4761628d347ada1b73063a607b3
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-05-02 17:02:20 +0530

    commit

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const BlogPage = () => {
@@ -6,11 +7,19 @@ const BlogPage = () => {
       <div className="relative h-full w-full bg-custom1-bg bg-cover bg-center text-white">
       <div className="absolute inset-0 bg-black bg-opacity-80" />
      <div className='relative '>
-          <div className=' h-[233px]'>
+          <div className=' h-[233px] flex flex-col justify-center absolute top-[230px] inset-0 bg-black bg-opacity-40 px-6'>
             <h4 className='text-[16px] text-[#0C487B] w-[142px] h-[24px]'>LATEST BLOGS</h4>
           <h1 className='text-[30px] font-black'>Blog & News</h1>
-          <div className='flex w-[361px] h-[156px]' >
-          <span className='w-[120px] h-[12px] sm:w-full'>Tech pyro</span><span className='text-[16px]'>is a tech hiring platform that helps recruiters and engineering managers effortlessly hire the best developers thanks to a powerful suite of virtual recruiting tools that help identify, assess, interview and engage developers.</span>
+          <div className='text-[16px]'>
+          <span className='w-[80px] font-bold'>TECH PYRO</span> is a tech hiring platform that helps recruiters and engineering managers effortlessly hire the best developers thanks to a powerful suite of virtual recruiting tools that help identify, assess, interview and engage developers.
+          </div>
+        </div>
+        <div className='absolute top-[480px] inset-0 bg-black bg-opacity-70'>
+          <div>
+            <Image src="/grp-img1.svg" width={368} height={216} alt="grp1" />
+          </div>
+          <div>
+            
           </div>
         </div>
      </div>
