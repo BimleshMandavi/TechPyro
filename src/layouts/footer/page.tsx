632a67bfccd6f2b2854df75cc527e1dff0ca1447
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2024-05-14 17:53:22 +0530

    commit

--- src/layouts/footer/page.tsx
+++ src/layouts/footer/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
-import Headroom from "react-headroom"
+
 const FooterPage = () => {
 
   const pathname = usePathname()
